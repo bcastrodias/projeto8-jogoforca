de509e524aa919246b0fbefcb0da5f56878e05ee
Bruno <b.castro.doute@gmail.com>
2022-11-27 23:04:15 -0300

    letras atualizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Letras from "./Letras";
+import { useState } from "react";
 
 function App() {
+  const [palavra, setPalavra] = useState();
+  const [chutes, setChutes] = useState([]);
+  const isGameRunning = palavra && chutes.length < 6;
+
   return (
     <div className="App">
-      <Letras />
+      <Letras
+        chutes={chutes}
+        setChutes={setChutes}
+        isGameRunning={isGameRunning}
+      />
     </div>
   );
 }
